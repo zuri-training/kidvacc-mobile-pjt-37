f21667f2c89f72d78b36c6ae5d9addb76fb1d162
cephas <c@CEPHAS-PETER>
2021-06-29 03:04:13 +0100

    Completed The Vaccine InfoBase Ui Started Design On The Vaccine Page Ui

--- app/src/main/java/com/zuri/kidvacc_mobile_pjt_37/ui/home/HomeFragment.kt
+++ app/src/main/java/com/zuri/kidvacc_mobile_pjt_37/ui/home/HomeFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
@@ -12,6 +11,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.zuri.kidvacc_mobile_pjt_37.R
 import com.zuri.kidvacc_mobile_pjt_37.ui.add_a_child.Add_A_Child
 import com.zuri.kidvacc_mobile_pjt_37.ui.infobase.Info_Base_Fragment
+import com.zuri.kidvacc_mobile_pjt_37.ui.vaccines.VaccineFragment
 
 class HomeFragment : Fragment() {
     private lateinit var homeViewModel: HomeViewModel
@@ -39,6 +39,15 @@ class HomeFragment : Fragment() {
             val fm: FragmentManager? = requireActivity().supportFragmentManager
             val fragmentTransaction: FragmentTransaction = fm!!.beginTransaction()
             fragmentTransaction.replace(R.id.home_frameLayout, Info_Base_Fragment())
+            fragmentTransaction.setReorderingAllowed(true)
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
+
+        root.findViewById<View>(R.id.vaccines).setOnClickListener {
+            val fm: FragmentManager? = requireActivity().supportFragmentManager
+            val fragmentTransaction: FragmentTransaction = fm!!.beginTransaction()
+            fragmentTransaction.replace(R.id.home_frameLayout, VaccineFragment())
             fragmentTransaction.setReorderingAllowed(true)
             fragmentTransaction.addToBackStack(null)
             fragmentTransaction.commit()
---
+++ app/src/main/java/com/zuri/kidvacc_mobile_pjt_37/ui/vaccines/VaccineFragment.kt
@@ -0,0 +1,25 @@
+package com.zuri.kidvacc_mobile_pjt_37.ui.vaccines
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.zuri.kidvacc_mobile_pjt_37.R
+
+class VaccineFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = VaccineFragment()
+    }
+
+    private lateinit var viewModel: VaccineViewModel
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        viewModel = ViewModelProvider(this).get(VaccineViewModel::class.java)
+        var root = inflater.inflate(R.layout.vaccine_fragment, container, false)
+        return root
+    }
+
+}
